5bf1b6fd39a4e5582d8ed37b7ae7871f035d543d
Indra <indra@ide.web.id>
2022-10-28 16:42:23 +0700

    set service_id in content

--- src/pkg/query/content.go
+++ src/pkg/query/content.go
@@ -7,9 +7,9 @@ import (
 
 func GetContent(serviceId int, name string) (model.Content, error) {
 	var content model.Content
-	sqlStatement := "SELECT value FROM contents WHERE service_id = ? AND name = ? LIMIT 1"
+	sqlStatement := "SELECT value, origin_addr FROM contents WHERE service_id = ? AND name = ? LIMIT 1"
 	db := database.Datasource.SqlDB()
-	err := db.QueryRow(sqlStatement, serviceId, name).Scan(&content.Value)
+	err := db.QueryRow(sqlStatement, serviceId, name).Scan(&content.Value, &content.OriginAddr)
 	if err != nil {
 		return content, err
 	}
