a0932f9701055a826a44796888126d000138d63c
“Indra <babekost.com@gmail.com>
2023-07-10 16:16:41 +0700

    reset queue

--- reset-queue.sh
+++ reset-queue.sh
@@ -1,4 +1,4 @@
-curl -X DELETE --user admin:admin http://ngxmp.com:15675/api/queues/%2f/Q_RENEWAL/contents
+curl -X DELETE --user admin:admin http://35.247.131.49:15675/api/queues/%2f/Q_RENEWAL/contents
 echo "\n"
 sleep 1
 
